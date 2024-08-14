8d29abc6f44c9e486bb91903ddda77f1cb31e3f2
Ruby Kendrick <kendrick.ruby@gmail.com>
2024-08-14 17:21:07 -0500

    changed Home view to have the logo

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -12,7 +12,7 @@ const TabIcon = ({ icon, color, name, focused }) => {
                 className="w-6 h-6"
             />
             <Text
-                className={`${focused ? 'font-psemibold' : 'font-pregular}'} text-xs`}>
+                className={`${focused ? 'font-psemibold' : 'font-pregular}'} text-xs `} style={{color:color}}>
                 {name}
             </Text>
         </View>
@@ -24,7 +24,15 @@ const TabsLayout = () => {
         <>
             <Tabs
                 screenOptions={{
-                    tabBarShowLabel: false
+                    tabBarShowLabel: false,
+                    tabBarActiveTintColor: '#FFA001',
+                    tabBarInactiveTintColor: '#CDCDE0',
+                    tabBarStyle: {
+                        backgroundColor: 'black',
+                        borderTopWidth: 1,
+                        borderTopColor: '2#32533',
+                        height: 84, 
+                    }
                 }}
             >
                 <Tabs.Screen
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Nail Tech</Text>
+      <StatusBar style="auto"/>
     </View>
   )
 }
+
 
 export default Home
